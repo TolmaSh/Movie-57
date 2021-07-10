c751f26c48bbd29f9f5dade8bee774f61d298ecc
TolmaSh <m0v4anua@gmail.com>
2021-07-10 22:15:01 +0300

    Updates

--- precache-manifest.e5df277b5b89e48a6dd2a0d9ba1c247e.js
+++ precache-manifest.e5df277b5b89e48a6dd2a0d9ba1c247e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "62e58566732bd83a6d9fb6b3b85850bf",
+    "revision": "8b480c20b7b07335fca18374c2c09a18",
     "url": "/Movie-57-React-Redux/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Movie-57-React-Redux/static/css/2.2a86faee.chunk.css"
   },
   {
-    "revision": "81581d8b824d5b60fbc7",
+    "revision": "1620c4916df8eae654ce",
     "url": "/Movie-57-React-Redux/static/css/main.2523266c.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Movie-57-React-Redux/static/js/2.fe040e74.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "81581d8b824d5b60fbc7",
-    "url": "/Movie-57-React-Redux/static/js/main.0a76c443.chunk.js"
+    "revision": "1620c4916df8eae654ce",
+    "url": "/Movie-57-React-Redux/static/js/main.ad15be51.chunk.js"
   },
   {
     "revision": "c250327607abba7c2ec7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Movie-57-React-Redux/precache-manifest.4bc109b16244481ac199f08ebb3b8de0.js"
+  "/Movie-57-React-Redux/precache-manifest.e5df277b5b89e48a6dd2a0d9ba1c247e.js"
 );
 
 self.addEventListener('message', (event) => {
